71e09a799b8ff0b6d2589f2a63d750e6a46579a1
Shahab <shahab.molouk@gmail.com>
2022-08-31 21:46:29 +0200

    Create Project

---
+++ Basic Submission/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Basic_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re \"accounting\" on you!");
+        }
+    }
+}
